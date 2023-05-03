b28605b734df93eeba1b21e0ba8dc6fbe3e25308
Gaurav Singh <gauravkarki038@gmail.com>
2023-05-03 09:54:59 +0530

    password

--- connect.js
+++ connect.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const db = mysql.createConnection({
     host: process.env.NODE_ENV == "localhost" ? "localhost" : process.env.DB_HOST,
     user: process.env.NODE_ENV == "localhost" ? "root" : process.env.DB_USER,
-    password: process.env.NODE_ENV == "localhost" ? "Ganesh7060@" : process.env.DB_PASSWORD,
+    password: process.env.NODE_ENV == "localhost" ? "......" : process.env.DB_PASSWORD,
     database: process.env.NODE_ENV == "localhost" ? "social" : process.env.DB_NAME,
     port: process.env.NODE_ENV == "localhost" ? 3306 : process.env.DB_PORT
 })
